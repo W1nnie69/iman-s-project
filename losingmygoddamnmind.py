bc44322b3f1ca49418e159fd44a2ec87c9d0fab5
W1nnie69 <danizye@gmail.com>
2023-01-08 18:16:09 +0800

    Update losingmygoddamnmind.py
    official ver 2

--- losingmygoddamnmind.py
+++ losingmygoddamnmind.py
@@ -1,22 +1,6 @@
-import numpy 
 import csv
 import time
 import os
-
-
-def fueloptionconvertion():
-    global x
-    fueldict = {
-        "Diesel":'1',
-        "Diesel-Electric":"2",
-        "Electric":"3",
-        "Petrol":"4",
-        "Petrol-CNG":"5",
-        "Petrol-Electric":"6",
-        "Petrol-Electric(Plug-in)":"7"
-    }
-    x = fueldict.get(foc)
-    print(x)
 
 
 
@@ -53,7 +37,6 @@ def gettingmeanvalueofselectedfueltype():
     rowprintappend = tuple(rowprintname) + tuple(rowprintsliced) #append fuel name and the values together, making a new tuple
     output = ', '.join(map(str, rowprintappend))       #removes the brackets
 
-
     row = read_csv_row(int(userinput)) 
     slicing = row[3:8]                            #picks values from 2008 to 2012
     sliced = tuple(map(int, slicing))
@@ -62,11 +45,15 @@ def gettingmeanvalueofselectedfueltype():
 
     meanvalue = total / len(sliced)
 
+    
+
     print("Showing data from 2008 to 2012")
     print("")
     print(output)
     print("")
     print("mean: ", meanvalue)
+
+    return meanvalue
 
 
 
@@ -74,18 +61,26 @@ def gettingmeanvalueofselectedfueltype():
 
 def menu():
     global option1 
+    os.system("cls")
+    time.sleep(0.5)
+    with open("annualmvpop_dataset.csv", "r") as f:
+        csvfile = csv.reader(f)
+        for line in csvfile:
+            print(line)
+
+    print("")
     print(" -------------------------------------------------------")
-    print("| 1)Full table                                          |")                      
+    print("| 1)Vehicle Population of fuel type from 2006 - 2018    |")                      
     print("| 2)Select fuel type to display                         |")
     print("| 3)Find which fuel type has decreased by at least 5%   |")
     print("| 4)Graphing shit                                       |")
     print(" -------------------------------------------------------")
 
-    option1 = input()
+    option1 = input("Please eneter which an option:" ) 
 
 #menu()
 
-option1 = "2" #temp code for testing
+option1 = '2'
 
 loop = True
 while loop == True:
@@ -114,11 +109,40 @@ while loop == True:
         print("")
 
         
-        gettingmeanvalueofselectedfueltype()
-    
+        x = gettingmeanvalueofselectedfueltype()
+        
+        y = int(x)
+
+        print("")
+
+        input("Press Enter to Continue")
+        
+        print("")
+
+        with open("annualmvpop_dataset.csv", "r") as f:  #This is the part that answers Q2 part B aka, finding other values that are higher than the mean above
+            csvfile = csv.reader(f, delimiter=",")       
+            next(csvfile)   
+            for i, row in enumerate(csvfile):           
+                slicing = row[3:8]
+                cutdata =  tuple(map(int, slicing))     
 
 
-        #output = ', '.join(map(str, meanvalue))
+           
+                for value in cutdata:
+
+                    if value > y:
+                        print(value)
+                    
+
+                
+
+
+                
+
+            
+        
+                
+
         
         
         break
